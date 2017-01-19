c7828eed45788f316ac2093d2b703f4cb212b18b
rubenwardy <rubenwardy@gmail.com>
2017-01-19 14:57:41 +0000

    Implement job workflow in Worker

---
+++ service/fetchjob.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const reposervers = require("./reposervers")
+
+function run(data) {
+	return new Promise(function(resolve, reject) {
+		resolve()
+	})
+}
+
+module.exports = run
--- service/index.js
+++ service/index.js
@@ -5,9 +5,40 @@ require("process").chdir(__dirname)
 const FarmerAPI = require("./farmerapi")
 const api = new FarmerAPI()
 
-api.getWork().then(function(r) {
-	console.log("r: ")
-	console.log(r);
-}).catch(function(e) {
-	console.log("e" + e)
-})
+function startJob(data) {
+	console.log("Starting job of type " + data.type)
+	var job
+	switch (data.type) {
+		case "fetch":
+			job = require("./fetchjob")
+			break
+		default:
+			return null
+	}
+
+	job().then(function(res) {
+		console.log("Job finished!")
+		console.log(res)
+		setTimeout(checkForJobs, 5000)
+	}).catch(function(e) {
+		console.log("Job failed!")
+		console.log(e)
+	})
+}
+
+function checkForJobs(data) {
+	console.log("Checking for jobs...")
+	api.getWork().then(function(job) {
+		if (job) {
+			startJob(job)
+		} else {
+			console.log("No jobs available")
+			setTimeout(checkForJobs, 5000)
+		}
+	}).catch(function(e) {
+		console.log("Error whilst checking for jobs")
+		setTimeout(checkForJobs, 5000)
+	})
+}
+
+setTimeout(checkForJobs, 5000)
